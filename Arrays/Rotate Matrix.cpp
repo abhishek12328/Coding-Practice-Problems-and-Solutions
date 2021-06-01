cb2ba14b3bdd1a3b29eaa0592d95c1da439e84f7
abhishek12328 <79263357+abhishek12328@users.noreply.github.com>
2021-06-02 01:13:53 +0530

    Update Rotate Matrix.cpp

--- Arrays/Rotate Matrix.cpp
+++ Arrays/Rotate Matrix.cpp
@@ -1,12 +1,22 @@
-int Solution::solve(vector<int> &a) {
-    sort(a.begin(),a.end());
-    if(a[a.size()-1]==0)
-    return 1;
-    for(int i=0;i<a.size();i++){
-        if(i<a.size()-1 && a[i]!=a[i+1] && a[i] == a.size()-i-1){
-            return 1;
+void Solution::rotate(vector<vector<int> > &A) {
+    
+    int i,j,temp,n = A.size(),k,newi,newj;
+    for(i=0;i<(A.size())/2;i++){
+        for(j=i;j<n-i-1;j++){
+            temp = A[i][j];
+            for(k=1;k<=4;k++){
+            newi = j;
+            newj = A.size()-1-i;
+            int temp1 = A[newi][newj];
+            A[newi][newj] = temp;
+            temp = temp1;
+            i = newi;
+            j = newj;
+            }
         }
-        
-    }
-    return -1;
+    } 
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
 }
