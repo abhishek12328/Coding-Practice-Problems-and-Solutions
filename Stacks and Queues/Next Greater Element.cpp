609a9bb9a2ada026ab9363470e229ddda6ccbc6a
abhishek12328 <79263357+abhishek12328@users.noreply.github.com>
2021-06-27 15:20:10 +0530

    Create Next Greater Element.cpp

---
+++ Stacks and Queues/Next Greater Element.cpp
@@ -0,0 +1,31 @@
+/*The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ ----------------------------------------
+ ----------------------------------------*/
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans(nums1.size(),-1);
+        stack<int> st;
+        map<int,int> m;
+        for(int i=0;i<nums2.size();i++){
+            while(!st.empty() && nums2[i]>st.top()){
+                m[st.top()] = nums2[i];
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            if(m[nums1[i]])
+            ans[i] = m[nums1[i]];
+        }
+        return ans;
+    }
+};
