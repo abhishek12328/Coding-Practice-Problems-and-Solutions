fc0b107c6320e5b8155160773025aeda85061269
abhishek12328 <79263357+abhishek12328@users.noreply.github.com>
2021-07-04 18:26:20 +0530

    Create Square root of integer.cpp

---
+++ Binary Search/Square root of integer.cpp
@@ -0,0 +1,17 @@
+int Solution::sqrt(int A) {
+    if(A==0 || A==1)return A;
+    int low=0,high=A/2;
+    int res=0;
+    while(low<=high){
+        long long mid=(low+high)/2;
+        if(mid*mid==A){
+            return mid;
+        }else if(mid*mid<A){
+            res=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return res;
+}
