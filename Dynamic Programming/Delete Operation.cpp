64b71309ca0547de41ce837b2a9c2daa6fb2657e
abhishek12328 <79263357+abhishek12328@users.noreply.github.com>
2021-07-14 19:35:53 +0530

    Similar to edit distance

---
+++ Dynamic Programming/Delete Operation.cpp
@@ -0,0 +1,51 @@
+/*Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+----------------------------------------------------------------
+----------------------------------------------------------------*/
+
+
+class Solution {
+public:
+
+    int editDistDP(string str1, string str2, int m, int n)
+{
+    // Create a table to store results of subproblems
+    int dp[m + 1][n + 1];
+ 
+    // Fill d[][] in bottom up manner
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            // If first string is empty, only option is to
+            // insert all characters of second string
+            if (i == 0)
+                dp[i][j] = j; // Min. operations = j
+ 
+            // If second string is empty, only option is to
+            // remove all characters of second string
+            else if (j == 0)
+                dp[i][j] = i; // Min. operations = i
+ 
+            // If last characters are same, ignore last char
+            // and recur for remaining string
+            else if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+ 
+            // If the last character is different, consider
+            // all possibilities and find the minimum
+            else
+                dp[i][j]
+                    = 1
+                      + min(dp[i][j - 1], // Insert
+                            dp[i - 1][j]); // Remove
+        }
+    }
+ 
+    return dp[m][n];
+}
+    
+    int minDistance(string word1, string word2) {
+        return editDistDP(word1,word2,word1.size(),word2.size());
+    }
+};
